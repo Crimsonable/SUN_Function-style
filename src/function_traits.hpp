a55231ca8a6e852477b4ed5da1a89e1c8627fce0
Sun Chen <542172229@qq.com>
2019-11-12 18:37:46 +0800

    完成了curry化操作

---
+++ src/function_traits.hpp
@@ -0,0 +1,93 @@
+#include <functional>
+#include <iostream>
+#include <string>
+#include <tuple>
+
+namespace _Sun {
+
+    template <class T>
+    constexpr std::string type_name() {
+        std::string name;
+#ifdef __clang__
+        name = __PRETTY_FUNCTION__;
+        return std::string(name.data() + 34, name.size() - 34 - 1);
+#endif
+#ifdef __GNUC__
+        name = __PRETTY_FUNCTION__;
+#if __cplusplus < 201402
+        return std::string(name.data() + 36, name.size() - 36 - 1);
+#else
+        return std::string(name.data() + 50, name.find(';', 49) - 50);
+#endif
+#endif
+#ifdef _MSC_VER
+        name = __FUNCSIG__;
+        return std::string(name.data() + 118, name.size() - 118 - 8);
+#endif
+    }
+
+    template <typename F>
+    struct function_traits;
+
+    template <typename Return, typename... Args>
+    struct function_traits<Return(Args...)> {
+        using func_type = std::function<Return(Args...)>;
+        using return_type = Return;
+        static constexpr std::size_t argsize = sizeof...(Args);
+        template <std::size_t I>
+        struct argument {
+            static_assert(I < argsize, "Index out of range");
+            using type = typename std::tuple_element<I, std::tuple<Args...>>::type;
+        };
+    };
+
+    //function pointer
+    template <class Return, class... Args>
+    struct function_traits<Return (*)(Args...)> : public function_traits<Return(Args...)> {};
+    //std::function
+    template <class Return, class... Args>
+    struct function_traits<std::function<Return(Args...)>> : public function_traits<Return(Args...)> {};
+    //back-end of Lambda and functor;class const member function
+    template <class Class, class Return, class... Args>
+    struct function_traits<Return (Class::*)(Args...) const> : public function_traits<Return(Args...)> {};
+    //functor and Lambda
+    template <typename Functor>
+    struct function_traits : public function_traits<decltype(&Functor::operator())> {};
+    //class member function
+    template <class Class, class Return, class... Args>
+    struct function_traits<Return (Class::*)(Args...)> : public function_traits<Return(Args...)> {};
+    //class member object
+    template <class Class, class Return>
+    struct function_traits<Return(Class::*)> : public function_traits<Return()> {};
+
+    template <class Func>
+    constexpr auto make_func(Func&& f) {
+        typename function_traits<std::remove_reference_t<Func>>::func_type _f = std::forward<decltype(f)>(f);
+        return std::forward<decltype(_f)>(_f);
+    }
+
+    template <class Func>
+    inline constexpr std::size_t argsize(Func&& f) {
+        return function_traits<std::remove_reference_t<Func>>::argsize;
+    }
+
+    template <class Func, std::size_t I>
+    struct arg_type {
+        using type = typename function_traits<std::remove_reference_t<Func>>::template argument<I>::type;
+    };
+}  // namespace _Sun
+
+/*double t(int a, int b) { return 2; }
+auto g = [](int& a, int b) -> double { return a + b + 0.2; };
+struct rua {
+    double s(int& r) { return 1; }
+    double k;
+};
+
+int main() {
+    using traits = _Sun::function_traits<decltype(&rua::k)>;
+    std::cout << _Sun::type_name<decltype(&rua::s)>() << std::endl;
+    system("pause");
+    return 1;
+}
+*/
--- src/tuple_tools.hpp
+++ src/tuple_tools.hpp
@@ -1,43 +1,53 @@
-#include<iostream>
-#include<string>
-#include<tuple>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <utility>
 
 namespace _Sun {
-	template<class Func, class Tuple, int N>
-	struct TupleHelper
-	{
-		static void func(Func f, const Tuple &_) {
-			TupleHelper<Func, Tuple, N - 1>::func(f, _);
-			f(std::get<N - 1>(_), N - 1);
-		}
-	};
-
-
-	template<class Func, class Tuple>
-	struct TupleHelper<Func, Tuple, 1>
-	{
-		static void func(Func f, const Tuple &_) {
-			f(std::get<0>(_), 0);
-		}
-	};
-
-	template<class Func, class ...Args>
-	void tuple_handler(Func f, const std::tuple<Args...> &_) {
-		TupleHelper<Func, decltype(_), sizeof...(Args)>::func(f, _);
-	}
-
-	template<class Func>
-	void tuple_handler(Func f, std::tuple<> &_) {
-          std::cout << "Trying to manipulate empty tuple!" << std::endl;
+    template <class Func, class Tuple, int N>
+    struct TupleHelper {
+        static void func(Func f, const Tuple &_) {
+            TupleHelper<Func, Tuple, N - 1>::func(f, _);
+            f(std::get<N - 1>(_), N - 1);
         }
+    };
 
-	template<class ...Args>
-	void print_tuple(const std::tuple<Args...> &_) {
-		std::cout << '(';
-		tuple_handler([](auto _, std::size_t index) {
-			if (index) std::cout << ", ";
-			std::cout << _;
-		}, _);
-		std::cout << ')';
-	}
-}
+    template <class Func, class Tuple>
+    struct TupleHelper<Func, Tuple, 1> {
+        static void func(Func f, const Tuple &_) {
+            f(std::get<0>(_), 0);
+        }
+    };
+
+    template <class Func, class... Args>
+    void tuple_handler(Func f, const std::tuple<Args...> &_) {
+        TupleHelper<Func, decltype(_), sizeof...(Args)>::func(f, _);
+    }
+
+    template <class Func>
+    void tuple_handler(Func f, std::tuple<> &_) {
+        std::cout << "Trying to manipulate empty tuple!" << std::endl;
+    }
+    //print tuple
+    template <class... Args>
+    void print_tuple(const std::tuple<Args...> &_) {
+        std::cout << '(';
+        tuple_handler([](auto _, std::size_t index) {
+            if (index) std::cout << ", ";
+            std::cout << _;
+        },
+                      _);
+        std::cout << ')';
+    }
+
+    template <class Func, class Tuple, std::size_t... I>
+    constexpr auto tuple_apply_impl(Func &&f, Tuple &&t, std::index_sequence<I...>) {
+        return std::invoke(std::forward<func>(f), std::get<I>(std::forward<Tuple>(t))...);
+    }
+
+    template <class Func, class Tuple>
+    constexpr auto tuple_apply(Func &&f, Tuple &&t) {
+        return tuple_apply_impl(std::forward<Func>(f), std::forward<Tuple>(t), std::make_index_sequence<std::tuple_size<std::remove_reference_t<Tuple>>::value>{});
+    }
+}  // namespace _Sun
